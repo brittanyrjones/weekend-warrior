686edf9d83e24f70adaf240423d8161a60a75460
Brittany Jones <brittany.r.jones@hotmail.com>
2018-02-25 21:16:08 -0800

    took out extra code that didnt work

--- 01-leap/leap.rb
+++ 01-leap/leap.rb
@@ -2,28 +2,14 @@
 #except every year that is evenly divisible by 100
 #unless the year is also evenly divisible by 400
 
-
-def leap_year(year)
+def leap_year?(year)
   if year % 400 == 0
-    true
+    return true
   elsif year % 100 == 0
-    false
+    return false
   elsif year % 4 == 0
-    true
+    return true
   else
-    false
+    return false
   end
 end
-
-
-
-
- def leap_year(year)
-   if year % 400 == 0 && year % 100 == 0
-     return false
-   elsif year % 400 == 0 && year % 4 == 0
-     return true
-   else
-     return false
-   end
- end
